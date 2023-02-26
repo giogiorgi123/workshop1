025179b536d90cfc591e2406dccb9ae2b42ec489
giogiorgi123 <gio.qarumidze123@gmail.com>
2023-02-26 14:51:32 +0400

    workshop1

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let name ="giorgi";
+console.log(name)
+let surname="qarumidze";
+console.log(surname);
+let gender ="man";
+console.log(gender);
+let  age = 24;
+console.log(age);
+
+let acsphraction ="9 ექსფრეშენი"
+console.log(acsphraction)
+
+let figure = 101%2;
+console.log(figure);
